a3d964bca72468a8a3fc04915bc706a53d1391f9
Damares Gaia <damaresgaia@gmail.com>
2025-09-22 09:39:50 -0300

    feat: exibe a lista de amigos na interface

--- app.js
+++ app.js
@@ -11,4 +11,17 @@ function adicionarAmigo() {
     
     amigos.push(amigo);
     document.getElementById('amigo').value = '';
+
+    exibirListaAmigos();
+}
+
+function exibirListaAmigos() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
 }
